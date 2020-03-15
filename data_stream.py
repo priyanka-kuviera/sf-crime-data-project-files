60b6f506d37e45f3050dc82674d0f6c584268895
Priyanka Kuviera <priyanka.kuviera@gmail.com>
2020-03-15 10:41:47 +0530

    second

--- data_stream.py
+++ data_stream.py
@@ -7,6 +7,21 @@ import pyspark.sql.functions as psf
 
 # TODO Create a schema for incoming resources
 schema = StructType([
+            StructField("crime_id",IntegerType(), True),
+            StructField("original_crime_type_name",StringType(),True),
+            StructField("report_date",TimestampType(),True),
+            StructField("call_date",TimestampType(),True),
+            StructField("offense_date",TimestampType(),True),
+            StructField("call_time",TimestampType(),True),
+            StructField("call_date_time"TimestampType(),True),
+            StructField("disposition",StringType(),True),
+            StructField("address",StringType(),True),
+            StructField("city",StringType(),True),
+            StructField("state",StringType(),True),
+            StructField("agency_id",IntegerType(),True),
+            StructField("address_type",StringType(),True),
+            StructField("common_location",StringType(),True)
+
 ])
 
 def run_spark_job(spark):
@@ -16,34 +31,74 @@ def run_spark_job(spark):
     # set up correct bootstrap server and port
     df = spark \
         .readStream \
+        .schema(schema) \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers","localhost:9092") \
+        .option("subscribe","data_topic") \
+        .option("startingOffsets","earliest") \
+        .option("maxOffsetPerTrigger",200) \
+        .option("maxRatePerPartition",10) \
+        .option("stopGracefullyOnShutdown","true") \
+        .load()
 
     # Show schema for the incoming resources for checks
     df.printSchema()
 
     # TODO extract the correct column from the kafka input resources
     # Take only value and convert it to String
-    kafka_df = df.selectExpr("")
+    '''kafka_df = df.selectExpr("CAST(crime_id AS STRING)", \
+                             "CAST(original_crime_type_name AS string)", \
+                             "CAST(report_date AS STRING)", \
+                             "CAST(call_date AS STRING)", \
+                             "CAST(offense_date AS STRING)", \
+                             "CAST(call_time AS STRING)", \
+                             "CAST(disposition AS STRING)", \
+                             "CAST(address AS STRING)", \
+                             "CAST(city AS STRING)", \
+                             "CAST(state AS STRING)", \
+                             "CAST(agency_id AS STRING)", \
+                             "CAST(address_type AS STRING)", \
+                             "CAST(common_location AS STRING)"
+                            )
+'''
+    kafka_df = df.selectExpr("CAST(value AS STRING)")
 
     service_table = kafka_df\
         .select(psf.from_json(psf.col('value'), schema).alias("DF"))\
         .select("DF.*")
 
     # TODO select original_crime_type_name and disposition
-    distinct_table = 
+    distinct_table = service_table \
+        .select('original_crime_type_name','disposition',"call_date_time") \
+        .distinct()
 
     # count the number of original crime type
-    agg_df = 
+    agg_df = distinct_table\
+        .select("original_crime_type_name", "call_date_time")\
+        .withWatermark("call_date_time", '60 minutes')\
+        .groupBy("original_crime_type_name")\
+        .count()
+    '''groupby on
+window of "call_date_time"
+"original_crime_type_name"
+and finally a count over it.'''
 
     # TODO Q1. Submit a screen shot of a batch ingestion of the aggregation
     # TODO write output stream
     query = agg_df \
+        .writeStream \
+        .format("console") \
+        .outputMode("Update") \
+        .trigger(processingTime="10 seconds") \
+        .option("truncate","false") \
+        .start()
 
 
     # TODO attach a ProgressReporter
     query.awaitTermination()
 
     # TODO get the right radio code json path
-    radio_code_json_filepath = ""
+    radio_code_json_filepath = "./radio_code.json"
     radio_code_df = spark.read.json(radio_code_json_filepath)
 
     # clean up your data so that the column names match on radio_code_df and agg_df
@@ -53,7 +108,7 @@ def run_spark_job(spark):
     radio_code_df = radio_code_df.withColumnRenamed("disposition_code", "disposition")
 
     # TODO join on disposition column
-    join_query = agg_df.
+    join_query = agg_df.join(radio_code_df, "disposition")
 
 
     join_query.awaitTermination()
--- kafka_server.py
+++ kafka_server.py
@@ -2,15 +2,16 @@ import producer_server
 
 
 def run_kafka_server():
-	# TODO get the json file path
-    input_file = ""
+    
+    # TODO get the json file path
+    input_file = "./police-department-calls-for-service.json"
 
     # TODO fill in blanks
     producer = producer_server.ProducerServer(
         input_file=input_file,
-        topic="",
-        bootstrap_servers="",
-        client_id=""
+        topic="data_topic",
+        bootstrap_servers="localhost:9092",
+        client_id="crimeData_clientID"
     )
 
     return producer
@@ -19,6 +20,19 @@ def run_kafka_server():
 def feed():
     producer = run_kafka_server()
     producer.generate_data()
+    
+def create_topic():
+    """Creates the topic with the given topic name"""
+    client = AdminClient({"bootstrap.servers": "localhost:9092"})
+    futures = client.create_topics(
+        [NewTopic(topic=TOPIC_NAME, num_partitions=1, replication_factor=1)]
+    )
+    for _, future in futures.items():
+        try:
+            future.result()
+        except Exception as e:
+            pass
+
 
 
 if __name__ == "__main__":
